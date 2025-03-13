42771fabff25209ec6fec1a5349b1061c115bb23
Rebeccakc <rkcromwell4@gmail.com>
2025-03-13 18:52:26 +0000

    form to submit task

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 // @ts-types="npm:@types/express"
 import express from "npm:express";
-
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(express.static("static"));
 
+app.use(express.urlencoded());
 app.get("/", function (_, res) {
   res.render("pages/index");
 });
@@ -67,6 +67,15 @@ app.get("/board", function (_req, res) {
   ];
 
   res.render("pages/board", { columns });
+});
+
+app.get("/create", function (_req, res) {
+  res.render("pages/create");
+});
+
+app.post("/create", function (req, res) {
+  const data = req.body;
+  console.log(data);
 });
 
 const port = Deno.env.get("PORT") || 8080;
